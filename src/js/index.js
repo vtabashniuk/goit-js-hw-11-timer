dffdff17687f00a327f9bafc10589fe35f36d3ac
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-07-25 10:37:01 +0300

    добавляет отрисовку таймера на странице

--- src/js/index.js
+++ src/js/index.js
@@ -3,46 +3,55 @@ import button from '../templates/button.hbs';
 
 const btnActions = [{ name: 'start' }, { name: 'stop' }, { name: 'clear' }];
 
-document.querySelector('body').insertAdjacentHTML('afterbegin', timerMarkup());
 document.querySelector('body').insertAdjacentHTML('afterbegin', button(btnActions));
+document.querySelector('body').insertAdjacentHTML('afterbegin', timerMarkup());
 
+function updateTimerMarkup({ days, hours, mins, secs }) {
+  const ddd = document.querySelector('[data-value=days]');
+  ddd.textContent = days;
+  const hhh = document.querySelector('[data-value=hours]');
+  hhh.textContent = hours;
+  const mmm = document.querySelector('[data-value=mins]');
+  mmm.textContent = mins;
+  const sss = document.querySelector('[data-value=secs]');
+  sss.textContent = secs;
+}
 let intervalId = null;
-const someDate = new Date('Jul 25, 2021');
+const someDate = new Date('Jul 26, 2021');
+
+function timeFormat(timeDiff) {
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+  return { days, hours, mins, secs };
+}
 
 document.querySelector('[data-action=start]').addEventListener('click', startTimer);
 
 document.querySelector('[data-action=stop').addEventListener('click', stopTimer);
 
-console.log(someDate.getTime());
+document.querySelector('[data-action=clear]').addEventListener('click', () => {
+  updateTimerMarkup({});
+});
 
-console.log((someDate - Date.now()) / 1000);
 function startTimer() {
+  document.querySelector('[data-action=start]').disabled = true;
+  document.querySelector('[data-action=start]').classList.add('isActive');
   intervalId = setInterval(() => {
     let time = someDate - Date.now();
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-    /*
-     * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-     * остатка % и делим его на количество миллисекунд в одном часе
-     * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-     */
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-    /*
-     * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-     * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-     */
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-    /*
-     * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-     * миллисекунд в одной секунде (1000)
-     */
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
-    console.log(`Days: ${days} | Hours: ${hours} | Minutes: ${mins} | Seconds: ${secs}`);
+    let timeToDisplay = timeFormat(time);
+    updateTimerMarkup(timeToDisplay);
+    console.log(
+      `Days: ${timeToDisplay.days} | Hours: ${timeToDisplay.hours} | Minutes: ${timeToDisplay.mins} | Seconds: ${timeToDisplay.secs}`,
+    );
   }, 1000);
 }
+
 function stopTimer() {
   clearInterval(intervalId);
+  document.querySelector('[data-action=start]').disabled = false;
+  document.querySelector('[data-action=start]').classList.remove('isActive');
 }
 // // clearInterval(intervalId);
